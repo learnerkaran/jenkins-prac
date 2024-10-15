de2f440dd229b20ba94b47edfbd384742e62da19
learnerkaran <168950203+learnerkaran@users.noreply.github.com>
2024-10-15 12:47:56 +0530

    asd

--- studentapp.groovy
+++ studentapp.groovy
@@ -1,1 +1,36 @@
-
+pipeline {
+  agent {
+    label 'karan-webserver'
+  }
+  stages {
+    stage('Pull') {
+      steps {
+       git 'https://github.com/learnerkaran/studentapp-ui.git'
+       echo 'Clone and Pull complete'
+      }
+    }
+    stage('Build') {
+      steps {
+        sh '''
+        sudo mvn clean package
+        sudo apt update
+        sudo apt install unzip -y
+        sudo curl -O https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.96/bin/apache-tomcat-9.0.96.zip
+        sudo unzip apache-tomcat-9.0.96'''
+        echo 'Build complete'
+      }
+    }
+    stage('Test') {
+      steps {
+       echo 'we are testing using sonarqube'
+      }
+    }
+    stage('Deploy') {
+      steps {
+        sh '''sudo mv target/*.war apache-tomcat-9.0.96/webapps/student.war
+        sudo bash apache-tomcat-9.0.96/bin/catalina.sh start'''
+        echo 'deployment complete'
+      }
+    }
+  }
+}
